668c49a2676b703dda1af3a6c2a7f7e54079dfeb
hafizihsanck <79069857+hafizihsanck@users.noreply.github.com>
2021-06-28 10:28:13 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -5,7 +5,7 @@ class Translation(object):
     START_TEXT = """
 Hello {} , I'am a simple youtube video link to telegram file or media uploader bot with permanent thumbnail support.
 
-Made by @FayasNoushad
+Made by @ihsan_bin_ahmd
 """
     HELP_TEXT = """
 <b><u>Link to Media or File</u></b>
@@ -20,22 +20,22 @@ Made by @FayasNoushad
 <b><u>Show Thumbnail</u></b>
 ➠ Send /showthumb to view custom thumbnail.
 
-Made by @FayasNoushad
+Made by @ihsan_bin_ahmd
 """
     ABOUT_TEXT = """
 - **Bot :** `YouTube Downloader`
-- **Creator :** [Fayas](https://telegram.me/TheFayas)
-- **Channel :** [Fayas Noushad](https://telegram.me/FayasNoushad)
+- **Creator :** [Hᴀʟᴇᴇᴍᴀ Jɪɴᴀ](https://telegram.me/Haleema_Jina)
+- **Channel :** [Moveis_hund](https://t.me/Moveis_hund)
 - **Credits :** `Everyone in this journey`
-- **Source :** [Click here](https://github.com/FayasNoushad/YouTube-Downloader)
+- **Source :** [Click here](https://t.me/Moveis_hund)
 - **Language :** [Python3](https://python.org)
 - **Library :** [Pyrogram v1.2.0](https://pyrogram.org)
 - **Server :** [Heroku](https://heroku.com)
 """
     START_BUTTONS = InlineKeyboardMarkup(
         [[
-        InlineKeyboardButton('Channel', url='https://telegram.me/FayasNoushad'),
-        InlineKeyboardButton('Feedback', url='https://telegram.me/TheFayas')
+        InlineKeyboardButton('Channel', url='https://t.me/Moveis_hund'),
+        InlineKeyboardButton('Feedback', url='https://telegram.me/ihsan_bin_ahmd')
         ],[
         InlineKeyboardButton('Help', callback_data='help'),
         InlineKeyboardButton('About', callback_data='about'),
@@ -56,7 +56,7 @@ Made by @FayasNoushad
         InlineKeyboardButton('Close', callback_data='close')
         ]]
     )
-    BLOCK_LIST_TEXT = "This url is blocked so I can not upload this URL.\n\nUse @FnURLUploadBot"
+    BLOCK_LIST_TEXT = "This url is blocked so I can not upload this URL.\n\nUse @Moveis_hund"
     FORMAT_SELECTION = """<b>Select the desired format:</b> <a href='{}'>file size might be approximate</a>
     
 Send your custum thumbnail if required.
@@ -65,11 +65,11 @@ You can use /delthumb to delete the auto-generated thumbnail."""
     BANNED_USER_TEXT = "<code>You are Banned!</code>"
     SET_CUSTOM_USERNAME_PASSWORD = """If you want to download premium videos, provide in the following format:
 URL | newfilename | username | password"""
-    DOWNLOAD_START = "<code>Downloading To My server Please Wait...</code>"    
+    DOWNLOAD_START = "<code>Downloading To My server Please Wait... Join @Moveis_hund</code>"    
     UPLOAD_START = "<code>Uploading into Telegram...</code>"
     AFTER_SUCCESSFUL_UPLOAD_MSG_WITH_TS = "Downloaded in {} seconds. \n\nUploaded in {} seconds."
     RCHD_TG_API_LIMIT = "Downloaded in {} seconds.\nDetected File Size: {}\nSorry. But, I cannot upload files greater than 1.95GB due to Telegram API limitations."
-    CUSTOM_CAPTION_UL_FILE = "<b>Join :-</b> @FayasNoushad"
+    CUSTOM_CAPTION_UL_FILE = "<b>Join :-</b> @Moveis_hund"
     SLOW_URL_DECED = "Gosh that seems to be a very slow URL. Since you were screwing my home, I am in no mood to download this file. Meanwhile, why don't you try this:==> https://shrtz.me/PtsVnf6 and get me a fast URL so that I can upload to Telegram, without me slowing down for other users."
     NO_VOID_FORMAT_FOUND = "<code>{}</code>"
     REPORT_SITE_TEXT = "<code>Sorry not uploading in this site here because this site is reporting site.</code>"
